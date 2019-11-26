b0ecaf7fa22d96769e6b058b4423cd245909a70b
Roberto Arruda <roberto0arruda@hotmail.com>
2019-11-26 14:06:54 -0400

    alter user structure

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const UserScrema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserScrema);
