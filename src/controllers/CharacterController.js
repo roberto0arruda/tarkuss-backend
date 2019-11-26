28949214f39aa0ee29685b4bb025fdec35bac2b6
Roberto Arruda <roberto0arruda@hotmail.com>
2019-11-26 14:21:57 -0400

    add imageUrl ext for character image

--- src/controllers/CharacterController.js
+++ src/controllers/CharacterController.js
@@ -4,6 +4,11 @@ module.exports = {
     async index(req, res) {
         const { data } = await axios.get('https://swapi.co/api/people/?format=json');
 
+        data.results.map(result => {
+            let characterIndex = result.url.split('/')[result.url.split('/').length - 2];
+            result.imageUrl = `//starwars-visualguide.com/assets/img/characters/${characterIndex}.jpg`;
+        });
+
         return res.json(data);
-    }
+    },
 }
