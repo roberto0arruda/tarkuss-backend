f11f3aabd2151871ea568b4c49c2c8808422363c
Roberto Arruda <roberto0arruda@hotmail.com>
2019-11-26 18:34:31 -0400

    get with page

--- src/controllers/CharacterController.js
+++ src/controllers/CharacterController.js
@@ -2,7 +2,10 @@ const axios = require('axios');
 
 module.exports = {
     async index(req, res) {
-        const { data } = await axios.get('https://swapi.co/api/people/?format=json');
+        const { page } = req.query;
+        let url = typeof page !== "undefined" ? `https://swapi.co/api/people/?format=json&page=${page}` : 'https://swapi.co/api/people/?format=json';
+
+        const { data } = await axios.get(url);
 
         data.results.map(result => {
             let characterIndex = result.url.split('/')[result.url.split('/').length - 2];
