acff20c2eca08da190be11c9caeacea44a52f211
Roberto Arruda <roberto0arruda@hotmail.com>
2019-11-26 14:06:18 -0400

    validate fields in login

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,15 +1,17 @@
 const User = require('../models/User');
 
 module.exports = {
-    async store(req , res) {
+    async store(req, res) {
         const { username, email, password } = req.body;
 
-        let user = await User.findOne({ email });
+        if (email && password) {
+            let user = await User.findOne({ email, password });
 
-        if (!user) {
-            user = await User.create({ username, email, password });
+            if (!user) {
+                user = { message: 'login ou password invalidos!' };
+                // user = await User.create({ username, email, password });
+            }
+            return res.json(user);
         }
-
-        return res.json(user);
     }
 };
